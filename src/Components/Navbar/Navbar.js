d993d9f7df621be0bc7a9954b6bfbdf209051b68
Farihaakter <yellowfariha@gmail.com>
2021-06-26 20:32:44 +0600

    added dropdown nav

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({toggle}) => {
     return (
         <nav className='flex lg:justify-center md:justify-center sm:justify-between justify-between h-20 items-center shadow-lg  w-full bg-gray-50 fixed z-10'>
             <Link to='/' className='lg:hidden md:hidden px-8 text-custom text-2xl text-yellow-600'>New Day</Link>
-            <div className='px-8 hover:text-pink-600 cursor-pointer md:hidden'>
+            <div className='px-8 hover:text-pink-600 cursor-pointer md:hidden' onClick={toggle}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h7" />
                 </svg>
